99d9c43eb53bffa47b272403eea2096b4df63e81
윤희연 <73658776+Yeon0105@users.noreply.github.com>
2023-04-22 18:30:57 +0900

    Create led.c

---
+++ GPIO/led.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<wiringPi.h>
+
+int main() {
+	if(wiringPiSetup() == -1)
+		return-1;
+	pinMode (0,OUTPUT);
+	pinMode (2,OUTPUT);
+
+	for( ; ;){
+		digitalWrite(2,0);
+		digitalWrite(0,1);
+		delay(1000);
+		digitalWrite(2,1);
+		digitalWrite(0,0);
+		delay(1000);
+	}
+return 0;
+}
